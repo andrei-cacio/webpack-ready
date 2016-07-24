f26e8e9d7e2c90934ef15b4675d8276310e91c85
Andrei Cacio <andrei.cacio@yahoo.com>
2016-07-24 17:28:52 +0300

    Added ES2015 support

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve('./src'),
+  entry: 'index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/assets',
+    path: 'build'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'src']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
