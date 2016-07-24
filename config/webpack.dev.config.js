5da7915d6aaa7b1e9fcaf15e1be43973eddbb3a3
Andrei Cacio <andrei.cacio@yahoo.com>
2016-07-24 18:39:28 +0300

    Added ES7 object spread support

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -29,7 +29,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: ['transform-object-rest-spread']
         }
       },
       {
